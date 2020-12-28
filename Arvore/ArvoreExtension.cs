403961697d5e652614fe3a9e99d1c84ff21c8fd3
Alesandro Fidelis <alesandro.fidelis@hbsis.com.br>
2020-12-28 00:11:08 -0300

    initial commit

---
+++ Arvore/ArvoreExtension.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Arvore
+{
+    public static class ArvoreExtension
+    {
+        public static void ConstruirEImprimirArvore(this int[] arvore)
+        {
+            var arvoreList = arvore.ToList();
+
+            (int raiz, int indexRaiz) = ObterRaizEIndexRaiz(arvoreList);
+            var galhosEsquerda = ObterGalhosEsquerdos(arvoreList, indexRaiz);
+            var galhosDireita = ObterGalhosDireitos(arvoreList, indexRaiz);
+
+            Console.WriteLine("\nRaiz: " + raiz);
+            Console.WriteLine("Galhos da esquerda -> " + string.Join(",", galhosEsquerda.Select(x => x).OrderByDescending(e => e)));
+            Console.WriteLine("Galhos da direita -> " + string.Join(",", galhosDireita.Select(x => x).OrderByDescending(e => e)));
+        }
+
+        private static List<int> ObterGalhosEsquerdos(List<int> arvoreList, int indexRaiz)
+        {
+            List<int> galhosEsquerda = new List<int>();
+            for (int i = 0; i < indexRaiz; i++)
+            {
+                galhosEsquerda.Add(arvoreList[i]);
+            }
+
+            return galhosEsquerda;
+        } 
+        
+        private static List<int> ObterGalhosDireitos(List<int> arvoreList, int indexRaiz)
+        {
+            List<int> galhosDireita = new List<int>();
+            for (int i = indexRaiz + 1; i < arvoreList.Count; i++)
+            {
+                galhosDireita.Add(arvoreList[i]);
+            }
+
+            return galhosDireita;
+        }
+
+        private static (int raiz, int indexRaiz) ObterRaizEIndexRaiz(List<int> arvoreList)
+        {
+            var raizIndex = arvoreList.IndexOf(arvoreList.Max());
+            var raiz = arvoreList[raizIndex];
+
+            return (raiz, raizIndex);
+        }
+    }
+}
---
+++ Arvore/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Arvore
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            int[] arvoreCenario1 = { 3, 2, 1, 6, 0, 5 };
+            int[] arvoreCenario2 = { 7, 5, 13, 9, 1, 6, 4 };
+
+            arvoreCenario1.ConstruirEImprimirArvore();
+            arvoreCenario2.ConstruirEImprimirArvore();
+
+            Console.ReadKey();
+        }
+    }
+}
